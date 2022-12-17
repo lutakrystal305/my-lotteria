11087883f87272cac49324c82b0118468132b2ef
Luta Krystal <nguyenvanthai305@gmail.com>
2022-12-17 21:25:55 +0700

    third commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,38 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import './Home.css';
-import ModalCustom from './ModalCustom';
-import classNames from 'classnames';
-import rewardImg from './images/reward.png'
+import React, { useState, useEffect } from "react";
+import "./Home.css";
+import ModalCustom from "./ModalCustom";
+import classNames from "classnames";
+import rewardImg from "./images/reward.png";
+import bg from "./images/bg.jpg";
+import cat from './images/cat.png'
+
+const numberOfRewards = 5;
 
 const Home = () => {
-  const [members, setMembers] = useState([]);
-  const [firstRewards, setFirstRewards] = useState([]);
-  const [secondRewards, setSecondRewards] = useState([]);
-  const [thirdRewards, setThirdRewards] = useState([]);
-  const [fourthRewards, setFourthRewards] = useState([]);
-  const [fifthRewards, setFifthRewards] = useState([]);
-  const [subRewards, setSubRewards] = useState([]);
+  const [rewards, setRewards] = useState({
+    spec: [],
+    first: [],
+    second: [],
+    third: [],
+    sub: [],
+  });
+  // const [firstRewards, setFirstRewards] = useState([]);
+  // const [secondRewards, setSecondRewards] = useState([]);
+  // const [thirdRewards, setThirdRewards] = useState([]);
+  // const [fourthRewards, setFourthRewards] = useState([]);
+  // const [fifthRewards, setFifthRewards] = useState([]);
+  // const [subRewards, setSubRewards] = useState([]);
   const [numberOfMember, setNumberOfMember] = useState(200);
   const [numberOfFist, setNumberOfFirst] = useState(1);
-  const [numberOfSecond, setNumberOfSecond] = useState(4);
-  const [numberOfThird, setNumberOfThird] = useState(10);
-  const [numberOfFourth, setNumberOfFourth] = useState(15);
-  const [numberOfFifth, setNumberOfFifth] = useState(20);
-  const [numberOfSub, setNumberOfSub] = useState(50);
+  const [numberOfSecond, setNumberOfSecond] = useState(1);
+  const [numberOfThird, setNumberOfThird] = useState(3);
+  const [numberOfFourth, setNumberOfFourth] = useState(5);
+  const [numberOfFifth, setNumberOfFifth] = useState(50);
+  // const [numberOfSub, setNumberOfSub] = useState(50);
   const [availMember, setAvilMember] = useState([]);
-  const [check, setCheck] = useState(6);
-  const [tempRes, setTempRes] = useState([])
-  const [openModal, setOpenModal] = useState(false)
-  const [startLotte, setStartLotte] = useState(false)
+  const [check, setCheck] = useState(numberOfRewards);
+  const [tempRes, setTempRes] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+  const [startLotte, setStartLotte] = useState(false);
+  const [isOpenModalSub, setIsOpenModalSub] = useState(false);
+  const [rewardSaved, setRewardSaved] = useState([]);
 
   const handleClickRandom = () => {
-    setStartLotte(true)
+    setStartLotte(true);
     setTimeout(() => {
-      handleRandom()
-      setOpenModal(true)
-      setStartLotte(false)
-    }, 3000)
-  }
+      handleRandom();
+      setOpenModal(true);
+      setStartLotte(false);
+    }, 3000);
+  };
 
   const handleRandom = () => {
     if (check <= 0) return;
@@ -54,11 +66,11 @@ const Home = () => {
       case 5:
         amount = numberOfFifth;
         break;
-      case 6:
-        amount = numberOfSub;
-        break;
+      // case 6:
+      //   amount = numberOfSub;
+      //   break;
       default:
-        amount = numberOfSub;
+        amount = numberOfFifth;
         break;
     }
     for (let i = 0; i < amount; i++) {
@@ -76,32 +88,46 @@ const Home = () => {
       }
     }
     if (check === 1) {
-      setFirstRewards(result);
-      localStorage.setItem('first', JSON.stringify(result));
+      setRewards({ ...rewards, spec: result });
+      localStorage.setItem("spec", JSON.stringify(result));
+    } else if (check === 2) {
+      setRewards({ ...rewards, first: result });
+      localStorage.setItem("first", JSON.stringify(result));
+    } else if (check === 3) {
+      setRewards({ ...rewards, second: result });
+      localStorage.setItem("second", JSON.stringify(result));
+    } else if (check === 4) {
+      setRewards({ ...rewards, third: result });
+      localStorage.setItem("third", JSON.stringify(result));
+    } else {
+      setRewards({ ...rewards, sub: result });
+      localStorage.setItem("sub", JSON.stringify(result));
     }
-    else if (check === 2) {
-
-    setSecondRewards(result);
-    localStorage.setItem('second', JSON.stringify(result));
-    }
-    else if (check === 3) {
-      setThirdRewards(result);
-      localStorage.setItem('third', JSON.stringify(result));
-    }
-    else if (check === 4) {
-      setFourthRewards(result);
-      localStorage.setItem('fourth', JSON.stringify(result));
-    }
-    else if (check === 5) {
-      setFifthRewards(result);
-      localStorage.setItem('fifth', JSON.stringify(result));
-    }
-    else { 
-      setSubRewards(result);
-      localStorage.setItem('sub', JSON.stringify(result));
-    }
-    setTempRes(result)
+    setTempRes(result);
     setCheck(check - 1);
+  };
+
+  const handleOpenModalSub = (x) => {
+    switch (x) {
+      case 0:
+        setRewardSaved(rewards.spec);
+        break;
+      case 1:
+        setRewardSaved(rewards.first);
+        break;
+      case 2:
+        setRewardSaved(rewards.second);
+        break;
+      case 3:
+        setRewardSaved(rewards.third);
+        break;
+      case 4:
+        setRewardSaved(rewards.sub);
+        break;
+      default:
+        break;
+    }
+    setIsOpenModalSub(true);
   };
 
   useEffect(() => {
@@ -110,28 +136,13 @@ const Home = () => {
         .fill()
         .map((_, i) => i + 1)
     );
-    const data = localStorage.getItem('data');
+    const data = localStorage.getItem("data");
     let result;
     if (data) {
       result = JSON.parse(data);
     }
-    if (result?.numberOfFist > 0) {
-      setNumberOfFirst(result.numberOfFist);
-    }
-    if (result?.numberOfSecond > 0) {
-      setNumberOfSecond(result.numberOfSecond);
-    }
-    if (result?.numberOfThird > 0) {
-      setNumberOfThird(result.numberOfThird);
-    }
-    if (result?.numberOfFourth > 0) {
-      setNumberOfFourth(result.numberOfFourth);
-    }
-    if (result?.numberOfFifth > 0) {
-      setNumberOfFifth(result.numberOfFifth);
-    }
-    if (result?.numberOfSub > 0) {
-      setNumberOfSub(result.numberOfSub);
+    if (result) {
+      setRewards(result);
     }
     // const first = JSON.parse(localStorage.getItem('first'))
     // setFirstRewards(result);
@@ -142,95 +153,157 @@ const Home = () => {
     // const sub = JSON.parse(localStorage.getItem('sub'))
   }, []);
   return (
-    <div className="Home d-flex flex-column position-relative">
+    <div className="Home d-flex justify-content-between align-items-center position-relative">
+      <div className="img-bg">
+        <img src={bg} alt="background" />
+      </div>
       <ModalCustom isOpen={openModal}>
         <div className="d-flex flex-column align-items-center position-relative">
-          <img src={rewardImg} alt="reward" className='imgModalReward' />
-          <h4 className='my-3 title-modal'>{check > 3 ? check + 'th' : check === 3 ? check + 'rd' : check === 2 ? check + 'nd' : check + "st"} Rewards</h4>
-        <div className="d-flex flex-wrap overflow-hidden justify-content-center">
-        {tempRes?.map((x, index) => (
-          <div key={index} className="element-1 mx-2 my-2">
-            {x}
+          <img src={rewardImg} alt="reward" className="imgModalReward" />
+          <h4 className="my-3 title-modal">
+            {check + 1 > 4
+              ? "Lucky"
+              : check === 3
+              ? check + "rd"
+              : check === 2
+              ? check + "nd"
+              : check === 1
+              ? check + "st"
+              : "Special"}{" "}
+            Rewards
+          </h4>
+          <div className="d-flex flex-wrap overflow-hidden justify-content-center">
+            {tempRes?.map((x, index) => (
+              <div key={index} className="element-1 mx-2 my-2">
+                {x}
+              </div>
+            ))}
           </div>
-        ))}
-        </div>
-        <button className='btn-modal' onClick={() => setOpenModal(false)}>OK</button>
+          <button className="btn-modal" onClick={() => setOpenModal(false)}>
+            OK
+          </button>
         </div>
       </ModalCustom>
-      <div className="lotteria-container d-flex flex-column position-relative align-items-center justify-content-end">
-        <h3 className='title-page'>Lotteria Random</h3>
-      <div className="lotteria">
-        <div className={classNames("circle-round-1", {'animate-circle-1' : startLotte})}></div>
-        <div className={classNames("circle-round-2", {'animate-circle-2' : startLotte})}></div>
-        <div className={classNames("circle-round-3", {'animate-circle-3' : startLotte})}></div>
-        <div className={classNames("circle-round-4", {'animate-circle-4' : startLotte})}></div>
-        <div className={classNames("circle-round-5", {'animate-circle-5' : startLotte})}></div>
-      </div>
-      
-      <button className='btn-random' onClick={handleClickRandom}> Random </button>
-      </div>
-      <div className="d-flex flex-row flex-wrap justify-content-between position-relative">
-      <div className="container-reward justify-content-start d-flex flex-column px-4 my-4 position-relative ">
-        <h3>1ST</h3>
-        <div className="d-flex flex-wrap">
-        {firstRewards?.map((x, index) => (
-          <div key={index} className="element-1 mx-2 my-2">
-            {x}
+      <ModalCustom isOpen={isOpenModalSub}>
+        <div className="d-flex flex-column align-items-center position-relative">
+          <img src={cat} alt="reward" className="imgModalReward" />
+          <h4 className="my-3 title-modal">
+          {rewardSaved?.length === numberOfFist ? "Special" : rewardSaved?.length === numberOfSecond ? "First" : rewardSaved?.length === numberOfThird ? "Second" : rewardSaved?.length === numberOfFourth ? "Third" : '"Lucky'}
+            Rewards
+          </h4>
+          <div className="d-flex flex-wrap overflow-hidden justify-content-center">
+            {rewardSaved?.length > 0 ? rewardSaved?.map((x, index) => (
+              <div key={index} className="element-1 mx-2 my-2">
+                {x}
+              </div>
+            )) : (
+            <h6>No Reward available</h6>)
+            }
           </div>
-        ))}
+          <button className="btn-modal" onClick={() => setIsOpenModalSub(false)}>
+            OK
+          </button>
         </div>
-      </div>
-      <div className="container-reward justify-content-start d-flex  flex-column px-4 my-4 ">
-        <h3>2nd</h3>
-        <div className="d-flex flex-wrap">
-        {secondRewards?.map((x, index) => (
-          <div key={index} className="element-1 mx-2 my-2">
-            {x}
+      </ModalCustom>
+      <div className="d-flex justify-content-between align-items-center position-relative w-100 h-100 body-container">
+        <div className="lotteria-container d-flex flex-column position-relative align-items-center justify-content-end">
+          <h3 className="title-page">Lotteria Random</h3>
+          <div className="lotteria">
+            <div
+              className={classNames("circle-round-1", {
+                "animate-circle-1": startLotte,
+              })}
+            ></div>
+            <div
+              className={classNames("circle-round-2", {
+                "animate-circle-2": startLotte,
+              })}
+            ></div>
+            <div
+              className={classNames("circle-round-3", {
+                "animate-circle-3": startLotte,
+              })}
+            ></div>
+            <div
+              className={classNames("circle-round-4", {
+                "animate-circle-4": startLotte,
+              })}
+            ></div>
+            <div
+              className={classNames("circle-round-5", {
+                "animate-circle-5": startLotte,
+              })}
+            ></div>
           </div>
-        ))}
-        </div>
-      </div>
 
-      <div className="container-reward justify-content-start d-flex flex-column px-4 my-4 ">
-        <h3>3RD</h3>
-        <div className="d-flex flex-wrap">
-        {thirdRewards?.map((x, index) => (
-          <div key={index} className="element-1 my-2 mx-2">
-            {x}
-          </div>
-        ))}
+          <button className="btn-random" onClick={handleClickRandom}>
+            {" "}
+            Random{" "}
+          </button>
         </div>
-      </div>
-      <div className="container-reward justify-content-start d-flex flex-column px-4 my-4 ">
-        <h3>4th</h3>
-        <div className="d-flex flex-wrap">
-        {fourthRewards?.map((x, index) => (
-          <div key={index} className="element-1 my-2 mx-2">
-            {x}
+        <div className="d-flex flex-row flex-wrap justify-content-between position-relative rewards">
+          <div className="container-reward justify-content-start d-flex flex-column px-4 my-4 position-relative mot-reward" onClick={() => handleOpenModalSub(0)}>
+            <h3>Special Reward</h3>
+            <div className="d-flex flex-wrap">
+              {/* {firstRewards?.map((x, index) => (
+                <div key={index} className="element-1 mx-2 my-2">
+                  {x}
+                </div>
+              ))} */}
+            </div>
           </div>
-        ))}
-        </div>
-      </div>
-      <div className="container-reward justify-content-start d-flex flex-column px-4 my-4 ">
-        <h3>5th</h3>
-        <div className="d-flex flex-wrap">
-        {fifthRewards?.map((x, index) => (
-          <div key={index} className="element-1 mx-2 my-2">
-            {x}
+          <div className="container-reward justify-content-start d-flex  flex-column px-4 my-4 hai-reward" onClick={() => handleOpenModalSub(1)}>
+            <h3>1st Rewards</h3>
+            <div className="d-flex flex-wrap">
+              {/* {/* {secondRewards?.map((x, index) => (
+                <div key={index} className="element-1 mx-2 my-2">
+                  {x}
+                </div>
+              ))} */}
+            </div>
           </div>
-        ))}
-        </div>
-      </div>
-      <div className="container-reward justify-content-start d-flex flex-column px-4 my-4 ">
-        <h3>Sub</h3>
-        <div className="d-flex flex-wrap">
-        {subRewards?.map((x, index) => (
-          <div key={index} className="element-1 mx-2 my-2">
-            {x}
+
+          <div className="container-reward justify-content-start d-flex flex-column px-4 my-4 ba-reward " onClick={() => handleOpenModalSub(2)}>
+            <h3>2nd Rewards</h3>
+            <div className="d-flex flex-wrap">
+              {/* {thirdRewards?.map((x, index) => (
+                <div key={index} className="element-1 my-2 mx-2">
+                  {x}
+                </div>
+              ))} */}
+            </div>
           </div>
-        ))}
+          <div className="container-reward justify-content-start d-flex flex-column px-4 my-4 bon-reward" onClick={() => handleOpenModalSub(3)}>
+            <h3>3rd Rewards</h3>
+            <div className="d-flex flex-wrap">
+              {/* {fourthRewards?.map((x, index) => (
+                <div key={index} className="element-1 my-2 mx-2">
+                  {x}
+                </div>
+              ))} */}
+            </div>
+          </div>
+          {/* <div className="container-reward justify-content-start d-flex flex-column px-4 my-4 nam-reward">
+            <h3>5th</h3>
+            <div className="d-flex flex-wrap">
+              {fifthRewards?.map((x, index) => (
+                <div key={index} className="element-1 mx-2 my-2">
+                  {x}
+                </div>
+              ))}
+            </div>
+          </div> */}
+          <div className="container-reward justify-content-start d-flex flex-column px-4 my-4 phu-reward " onClick={() => handleOpenModalSub(4)}>
+            <h3>Lucky Rewards</h3>
+            <div className="d-flex flex-wrap">
+              {/* {subRewards?.map((x, index) => (
+                <div key={index} className="element-1 mx-2 my-2">
+                  {x}
+                </div>
+              ))} */}
+            </div>
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
